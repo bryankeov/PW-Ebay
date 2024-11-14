ec29b1f327570f75740e09136dd8244fa2558b99
Bryan K <suprnewb@gmail.com>
2024-11-14 18:05:13 +1100

    Add search test

--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test"
+import { text } from "stream/consumers"
 
 test.beforeEach(async({page}) => {
     await page.goto("https://www.ebay.com.au/")
@@ -10,14 +11,20 @@ test.describe("Testing page load and navigation", async() => {
     })
 
     test("Navigating to sell", async ({page}) => {
-        await page.pause()
         await page.locator(`a[_sp="m570.l1528"]`).click()
         await expect(page.getByText("Turn your passion into profit")).toBeVisible()
     })
 
     test("Navigating to help", async ({page}) => {
-        await page.pause()
         await page.locator(`a[_sp="m570.l1545"]`).click()
         await expect(page.getByText("How can we help you today?")).toBeVisible()
+    })
+})
+
+test.describe("Test search function", async () => {
+    test("Search for hat", async ({page}) => {
+        await page.locator(`input#gh-ac`).fill("hat")
+        await page.getByRole('button', { name: 'Search' }).click()
+        await expect(page.getByText("results for hat")).toBeVisible()
     })
 })
